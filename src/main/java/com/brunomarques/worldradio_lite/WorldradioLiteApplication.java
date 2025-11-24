e2f0fa40a670b7669bafb387845b51be1c118d44
Bruno Marques <brunoa879@gmail.com>
2025-11-24 20:57:45 -0300

    Initial Spring Boot setup for WorldRadio Lite

---
+++ src/main/java/com/brunomarques/worldradio_lite/WorldradioLiteApplication.java
@@ -0,0 +1,13 @@
+package com.brunomarques.worldradio_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldradioLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldradioLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunomarques/worldradio_lite/WorldradioLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomarques.worldradio_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldradioLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
